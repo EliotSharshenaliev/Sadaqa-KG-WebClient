6a012bde21dc545c189116894aceb5ad9122aa10
Marat Sharshenaliev <sharshenaliev.03@gmail.com>
2023-10-15 21:34:58 +0600

    Added routing and protected routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ModalProvider} from "../context/profile_modal";
 import {BrowserRouter} from "react-router-dom";
 import {AuthProvider} from "../context/auth_context";
-import Router from "../router";
+import ProtectedRouter from "../router";
 
 
 function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <ModalProvider>
-                    <Router/>
-                </ModalProvider>
+                <ProtectedRouter/>
             </AuthProvider>
         </BrowserRouter>
     );
