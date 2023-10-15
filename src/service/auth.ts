198229e7f03f660d0bc5c1c25e932ade527e0030
Marat Sharshenaliev <sharshenaliev.03@gmail.com>
2023-10-15 21:35:55 +0600

    new class AuthService methods:  - login  - getCurrentUser  - logout  - deleteCurrentUser  - getOptionUpdate

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,19 +1,61 @@
 import {Instance} from "./index";
+import {USER_IN_STORAGE} from "../shared/config";
 import {User} from "../shared/types/user";
-import {Token} from "../shared/types/token";
+import authHeader from "./auth-header";
 
-export const getUser = async (token: Token): Promise<User | null> => {
-    try {
-        const response = await Instance.get(
-            "/auth/user/",
-            {
-                headers: {
-                    Authorization: `Bearer ${token.access}`
-                }
+
+class AuthService {
+    async login(username: string, password: string) {
+        try {
+            const response = await Instance.post("/auth/login/", {
+                username,
+                password
+            })
+            if (response.data.access) {
+                localStorage.setItem(USER_IN_STORAGE, JSON.stringify(response.data));
             }
-        )
-        return response.data
-    }catch (e){
-        throw e
+            return response.data
+        } catch (e) {
+            throw e
+        }
+    }
+
+    logout() {
+        localStorage.removeItem(USER_IN_STORAGE);
+    }
+
+    async getCurrentUser(): Promise<User> {
+        try {
+            const response = await Instance.get("/auth/user", {
+                headers: authHeader()
+            })
+            return response.data
+        } catch (e) {
+            throw e
+        }
+    }
+
+    async deleteCurrentUser() {
+        try {
+            await Instance.delete("/auth/user", {
+                headers: authHeader()
+            })
+            await this.logout()
+        } catch (e) {
+            throw e
+        }
+    }
+
+
+    async getOptionUpdate() {
+        try {
+            await Instance.options("/auth/user", {
+                headers: authHeader()
+            })
+        } catch (e) {
+            throw e
+        }
     }
 }
+
+export default new AuthService();
